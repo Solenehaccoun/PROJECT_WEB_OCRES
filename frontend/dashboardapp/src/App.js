9036831b28f0f1312da8fa034098feaca5d3113a
AliceCabane <63549730+AliceCabane@users.noreply.github.com>
2020-11-29 12:36:03 +0100

    widget 3 pie

--- frontend/dashboardapp/src/App.js
+++ frontend/dashboardapp/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Widget1 from './Widgets/Widget1';
 import Widget2 from './Widgets/Widget2';
+import Widget3 from './Widgets/Widget3';
 
 function App() {
   return (
@@ -10,8 +11,10 @@ function App() {
       <h1>Mes Widgets</h1>
       <div><Widget1/></div>
       <div><Widget2/></div>
+      <div><Widget3/></div>
     </div>
   );
 }
 
 export default App;
+
