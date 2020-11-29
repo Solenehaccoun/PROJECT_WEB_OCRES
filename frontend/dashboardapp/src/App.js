31604a0ed968320bf9e2a588a3f0ae71860827c7
Solenehaccoun <63588267+Solenehaccoun@users.noreply.github.com>
2020-11-29 12:17:14 +0100

    piechart + barchart

--- frontend/dashboardapp/src/App.js
+++ frontend/dashboardapp/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { PieChart } from 'recharts';
 import './App.css';
 
+import BarCharts from './Widgets/BarCharts';
+import Piechart from './Widgets/Piechart';
 import Widget1 from './Widgets/Widget1';
 import Widget2 from './Widgets/Widget2';
+
+
+
+
 
 function App() {
   return (
@@ -10,6 +17,11 @@ function App() {
       <h1>Mes Widgets</h1>
       <div><Widget1/></div>
       <div><Widget2/></div>
+      <div><BarCharts/></div>
+      <div><Piechart/></div>
+     {/* <div id="timer-example"></div> */}
+    
+
     </div>
   );
 }
---
+++ frontend/dashboardapp/src/Widgets/BarCharts.js
@@ -0,0 +1,50 @@
+import React, { PureComponent } from 'react';
+import {
+  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
+} from 'recharts';
+
+
+class BarCharts extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            chartData:  [
+                {name: 'Ariana', value: 56600},
+                {name:'Dua Lipa', value: 1500},
+                {name:'Black Pumas', value: 2000},
+                {name:'Tokyo Hotel', value: 800},
+              
+                ]
+            }
+        }
+    
+
+    render() {
+        return (
+            <ResponsiveContainer>
+            <div className="chart">
+                        <BarChart
+                            width={500}
+                            height={300}
+                            data={this.state.chartData}
+                            margin={{
+                            top: 5, right: 30, left: 20, bottom: 5,
+                            }}
+                            barSize={20}>
+
+                    <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
+                    <YAxis /> 
+                    <Tooltip />
+                    <Legend />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Bar dataKey="value" fill="#8884d8" background={{ fill: '#eee' }} />
+                </BarChart>
+            </div>
+            </ResponsiveContainer>
+        );
+    }
+
+}
+
+export default BarCharts;
