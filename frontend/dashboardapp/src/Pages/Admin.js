e0d79e1cb27199a9cfc3cc1017f55087945abce0
AliceCabane <63549730+AliceCabane@users.noreply.github.com>
2020-11-29 20:06:13 +0100

    form

--- frontend/dashboardapp/src/Pages/Admin.js
+++ frontend/dashboardapp/src/Pages/Admin.js
@@ -1,11 +1,10 @@
 import React from 'react';
-//import Form from '../Composants/Form';
+import Form from '../Composants/Form';
 
 function Admin() {
   return (
     <div className='admin'>
-      <h1>Admin</h1>
-      
+      <Form/>
     </div>
   );
 }
